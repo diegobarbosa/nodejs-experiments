2d5b169059156fe352f10a3c1b3392d0d7ae5a0c
diego barbosa <bob.diego1@gmail.com>
2018-09-16 01:01:35 -0300

    Add tests a calculadora.subtrair

--- calculator/calculadora.spec.js
+++ calculator/calculadora.spec.js
@@ -3,8 +3,22 @@ var assert = require('assert');
 
 describe('Calculadora', function() {
     describe('#somar()', function() {
+      
       it('deve retornar 3 quando 2 e 1 são informados', function() {
         assert.equal(calculadora.somar(2,1), 3);
       });
+
     });
+
+    describe('#subtrair()', function() {
+      
+      it('deve retornar 1 quando 2 e 1 são informados', function() {
+        assert.equal(calculadora.subtrair(2,1), 1);
+      });
+
+    });
+
+
+
+
   });
